11290ec2d50b456fe862cbc07a990145506a8ed8
Naren Dasan <naren@narendasan.com>
2019-03-07 14:52:18 -0800

    Adds a dockerfile to compile for qnx, support for JPEG Turbo on aarch64-qnx
    Signed-off-by: Naren Dasan <narens@nvidia.com>

--- dali/image/jpeg_mem.cc
+++ dali/image/jpeg_mem.cc
@@ -18,7 +18,7 @@ limitations under the License.
 // This file defines functions to compress and uncompress JPEG data
 // to and from memory, as well as some direct manipulations of JPEG string
 
-#if !defined(__AARCH64_QNX__)
+//#if !defined(__AARCH64_QNX__)
 #include "dali/image/jpeg_mem.h"
 #include <setjmp.h>
 #include <cstring>
@@ -790,4 +790,4 @@ string Compress(const void* srcdata, int width, int height,
 }  // namespace jpeg
 #endif
 }  // namespace dali
-#endif
+//#endif
